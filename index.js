d2f7cf24c6c22f503b6f74d2e10e51b9d6b99f0b
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-11-20 11:08:02 +0530

    Done with some apis

--- index.js
+++ index.js
@@ -49,18 +49,48 @@ client
   .connect()
   .then(() => {
     console.log("Connected successfully to server");
-    const postColection = client.db("database").collection("posts");
-    const userColection = client.db("database").collection("users");
+    const postCollection = client.db("database").collection("posts");
+    const userCollection = client.db("database").collection("users");
 
+    app.get("/user", async (req, res) => {
+      const user = await userCollection.find().toArray();
+      res.send(user);
+    });
+    app.get("/loggedInUser", async (req, res) => {
+      const email = req.query.email;
+      const user = await userCollection.find({ email: email }).toArray();
+      res.send(user);
+    });
     app.get("/post", async (req, res) => {
-      const posts = await postColection.find({}).toArray();
-      res.send(posts);
+      const post = (await postCollection.find().toArray()).reverse();
+      res.send(post);
+    });
+    app.get("/userPost", async (req, res) => {
+      const email = req.query.email;
+      const post = (
+        await postCollection.find({ email: email }).toArray()
+      ).reverse();
+      res.send(post);
+    });
+
+    app.post("/register", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
     });
 
     app.post("/post", async (req, res) => {
       const post = req.body;
-      const result = await postColection.insertOne(post);
-      console.log(result);
+      const result = await postCollection.insertOne(post);
+      res.send(result);
+    });
+
+    app.patch("/userUpdates/:email", async (req, res) => {
+      const filter = req.params;
+      const profile = req.body;
+      const options = { upsert: true };
+      const updateDoc = { $set: profile };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
       res.send(result);
     });
   })
