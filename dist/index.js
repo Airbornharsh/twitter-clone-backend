4c33fbbc20d1059965037fcda72b83b600643d82
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-11-23 13:12:08 +0530

    Added Routes

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,7 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const cors_1 = __importDefault(require("cors"));
 const DbConfig_1 = __importDefault(require("./config/DbConfig"));
+const Route_1 = __importDefault(require("./routes/Route"));
 const app = (0, express_1.default)();
 const router = express_1.default.Router();
 const port = process.env.PORT || 5000;
@@ -17,8 +18,8 @@ app.use(express_1.default.json());
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
-// app.use("/api", router);
-// Route(router);
+app.use("/api", router);
+(0, Route_1.default)(router);
 // client
 //   .connect()
 //   .then(() => {
